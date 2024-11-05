08c88bbc3fed335f60101d226f94a23396d19e71
yanghyeonjin <guswls7081@gmail.com>
2024-11-05 09:35:25 +0900

    init boot project

---
+++ src/test/java/com/ohgiraffers/elasticbeans_boot/ElasticbeansBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.elasticbeans_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticbeansBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
