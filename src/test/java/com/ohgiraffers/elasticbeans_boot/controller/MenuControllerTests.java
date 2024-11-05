bea712382002ae581e75fad2b31489981d3361aa
yanghyeonjin <guswls7081@gmail.com>
2024-11-05 12:18:57 +0900

    deploy.yml에 rds 정보 추가

--- src/test/java/com/ohgiraffers/elasticbeans_boot/controller/MenuControllerTests.java
+++ src/test/java/com/ohgiraffers/elasticbeans_boot/controller/MenuControllerTests.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,6 +24,16 @@ class MenuControllerTests {
     public void healthCheckTest() throws Exception {
         mockMvc.perform(get("/health"))
                 .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @DisplayName("2번 메뉴 있는지 확인")
+    @Test
+    public void findByMenuCodeTest() throws Exception {
+        mockMvc.perform(get("/menus/2"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.menuName").value("우럭스무디"))
                 .andDo(print());
     }
 }
